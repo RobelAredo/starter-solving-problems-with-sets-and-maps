90b587f43442182dbf17d7aaa1ec91fe6476da57
Robel Aredo <ichabodvancha@yahoo.com>
2022-01-04 14:10:17 -0600

    solve problems with sets and maps

--- src/sameLetters/index.js
+++ src/sameLetters/index.js
@@ -2,6 +2,17 @@
  * return a Map of the unique letters and arrays of words that are made up of those letters
  * @param {array} words An array  of words
  */
-function sameLetters(words) {}
+function sameLetters(words) {
+  const results = new Map();
+
+  for (let word of words) {
+    const key = [... new Set(word.toLowerCase())].sort().join("");
+
+    if (!results.has(key)) results.set(key, [word]);
+    else results.get(key).push(word);
+  }
+
+  return results;
+}
 
 module.exports = sameLetters;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,41 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+
+// function sumPairs(A, N) {
+//   const pairs = {};
+//   const solution = []
+//   for (let e of A) {
+//     const diff = N - e;
+//     if (pairs[e]) {
+//       solution.push([diff, e]);
+//     }
+//     else pairs[diff] = true;
+//   }
+//   return solution;
+// }
+
+function sumPairs(A, N) {
+  const numbers = new Map();
+  const solution = new Map();
+
+  for (let e of A) {
+    numbers.set(e);
+  }
+
+  for (let e of A) {
+    const diff = N - e;
+
+    if (diff !== e) {
+      if (numbers.has(diff)) {
+        const small = Math.min(e, diff);
+        const big = Math.max(e, diff);
+        solution.set(small, big);
+      }
+    }
+  }
+
+  return [...solution]
+}
 
 module.exports = sumPairs;
